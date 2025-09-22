6cf412201e17e2ddf5e2fe5c6ff8dc754b991fdc
Kien Nguyen <kiennguyen3112@gmail.com>
2025-09-22 11:29:51 +0700

    update

---
+++ bai_7/script.js
@@ -0,0 +1,34 @@
+// khai bao bien va gan gia tri (let, var)
+let age = 20;
+var name = "Nguyen Xuan Kien";
+
+// in ra site html
+document.writeln(name + "<br>");
+document.writeln(age + "<br>");
+
+//TOAN TU
+//arithmetic operators: +,-,*,/,%,...
+//logic operators: >,>=,<,=<,!=,...
+let num1 = 20;
+let num2 = "20";
+
+document.writeln(num1 == num2);
+
+//DATA TYPES
+
+let data = {
+    name: "XuanKien",
+    age: 20,
+    gender: 1,
+    class: "CNTT",
+    address: {
+        road: "An Duong Vuong",   
+    }
+}
+
+document.writeln("Nam sinh: " + (2025-data.age));
+
+
+
+
+
